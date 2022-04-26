d815c61f3ae084501bb542ffa8d23d02d1c6e031
David BC <david.branacampos@cognizant.com>
2022-04-26 08:24:15 +0200

    Initial Commit

---
+++ RaboBank/RaboBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaboBankApp.swift
+//  RaboBank
+//
+//  Created by m_944879 on 26/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RaboBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

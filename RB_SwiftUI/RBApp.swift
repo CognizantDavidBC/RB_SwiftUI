68fe451decb5e907d38b462a1d5ff25cec87b5fb
David BC <david.branacampos@cognizant.com>
2022-05-06 08:08:47 +0200

    Updated name

--- RB_SwiftUI/RBApp.swift
+++ RB_SwiftUI/RBApp.swift
@@ -1,14 +1,7 @@
-//
-//  RaboBankApp.swift
-//  RaboBank
-//
-//  Created by m_944879 on 26/4/22.
-//
-
 import SwiftUI
 
 @main
-struct RaboBankApp: App {
+struct RBApp: App {
     @StateObject var viewModel = ViewModel()
     
     var body: some Scene {
